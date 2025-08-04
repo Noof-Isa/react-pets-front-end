b1f311e987d3110fef01335de400ddcc2e2a5ab8
Noof-Isa <noofisa786@gmail.com>
2025-08-04 11:43:47 +0300

    part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// // src/App.jsx
+// import * as petService from './services/petService.js'
 
-function App() {
-  const [count, setCount] = useState(0)
+// import { useState, useEffect } from 'react';
+
+// const App = () => {
+//   const [pets, setPets] = useState([]);
+
+//   return <h1>Hello world!</h1>;
+// };
+
+// export default App;
+import * as petService from './services/petService'
+import { useEffect, useState } from 'react'
+import PetList from './components/PetList/PetList'
+import PetDetail from './components/PetDetail/PetDetail'
+
+const App = () => {
+  const [pets, setPets] = useState([])
+  const [selected, setSelected] = useState(null)
+  
+  useEffect(() => {
+    const fetchPets = async() => {
+      try {
+        const fetchedPets = await petService.index()
+        setPets(fetchedPets)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchPets()
+  }, [])
+
+  const handleSelect = (pet) => {
+    setSelected(pet)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <PetList pets={pets} handleSelect={handleSelect} />
+      <PetDetail selected={selected} />
     </>
   )
 }
